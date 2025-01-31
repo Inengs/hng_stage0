c8fe7b9ea39dbabf23713f04f932c1ed13ec191f
Inengiye Emmanuel <inengiyeemmanuel@gmail.com>
2025-01-31 19:54:14 +0100

    changed it back

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
+app.get('/details', (req, res) => {
     try {
         const email = 'inengiyeemmanuel@gmail.com';
         const dateandTimeNow = new Date().toISOString();
