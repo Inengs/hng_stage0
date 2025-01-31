1b60d49fe610e3dfd2a9f2240b6e565796de61d4
Inengiye Emmanuel <inengiyeemmanuel@gmail.com>
2025-01-31 02:59:41 +0100

    second push

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const port = 3000;
+const cors = require('cors');
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+
+app.use(cors());
 
 app.get('/details', (req, res) => {
+    try {
+        const email = 'inengiyeemmanuel@gmail.com';
+        const dateandTimeNow = new Date().toISOString();
+        const githubRepo = 'https://github.com/Inengs/hng_stage0';
 
+        const response = {
+            email: email,
+            currentTime: dateandTimeNow,
+            githubRepo: githubRepo,
+        }
+
+        res.status(200).json(response);
+    } catch (err) {
+        res.status(401).json({ error: "task failed" })
+    }
+})
+
+app.listen(port, () => {
+    console.log(`app listening on port ${port}`)
 })
